609af6d20b3cda6610e62d827730497d7a3199c5
Marco Ferreira <marcoasf.com@gmail.com>
2022-11-08 14:40:05 -0600

    starting inquierer from scratch.... couldnt fix issue

--- db/shema.sql
+++ db/shema.sql
@@ -1,50 +1,73 @@
-DROP DATABASE IF EXISTS TheOffice_db;
-CREATE DATABASE TheOffice_db;
+DROP DATABASE IF EXISTS employee_trackerDB;
 
-use TheOffice_db;
+CREATE DATABASE employee_trackerDB;
 
+USE employee_trackerDB;
+
+-- DEPARTMENT TABLE ----
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30)
+ 
 );
+-- DEPARTMENT TABLE ----
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary decimal NOT NULL,
-  department_id Integer
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
-CREATE TABLE employees (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER, 
-    manager_id Integer
+-- EMPLOYEE ROLE TABLE ----
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
 );
+
+-- DEPARTMENT SEEDS -----
 INSERT INTO department (name)
-VALUES 
-('CEO'),
-('Sales'),
-('Accounting'),
-("HR");
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Finance");
 
+
+-- EMPLOYEE ROLE SEEDS -------
 INSERT INTO role (title, salary, department_id)
-VALUES
-('CEO', 150000, 1),
-('Sales Lead',100000,2 ),
-('Sales Intern',30000,2 ),
-('Accounting', 80000,3),
-('HR', 50000,4);
+VALUE ("Regional Manager", 150000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Assistant to the manager", 250000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Customer Service", 120000, 1);
 
 
+-- EMPLOYEE SEEDS -------
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Michael", "Scott", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Dwight", "Schrute", 1, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jim","Halpert",1,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Pam", "Halpert", 1, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Creed", "Creed", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Kelly", "Kapour", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Ryan", "DontRemember", 2, 5);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES 
-('Michael', 'Scott', 1, null),
-('Dwight', 'Schrute', 2, 1),
-('Jim', 'Halpert', 3, 1),
-('Pam', 'Halpert', 4, 3),
-('Kelly', 'Kapour', 5, 1);
-
-SELECT * FROM TheOffice_db.role;
-SELECT * FROM TheOffice_db.employees;
-SELECT * FROM TheOffice_db.department;
+-- SELECTING FOR CREATING 
+--TABLES IN OUR SQL WORKBENCH 
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
